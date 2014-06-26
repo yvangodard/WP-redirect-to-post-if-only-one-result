098ad734fdb8babfb35f38f0b9a680d5a11d5ac3
Yvan GODARD <godardyvan@gmail.com>
2014-06-26 16:21:40 +0200

    Version 1.2

--- wp-redirect-to-post-if-only-one-result.php
+++ wp-redirect-to-post-if-only-one-result.php
@@ -3,10 +3,11 @@
   Plugin Name:  Rediriger vers l’article si la recherche ou le tag ne retourne qu’un résultat
   Description:  Cette astuce permet d'améliorer la navigation de votre site en redirigeant vers le seul article d'une recherche fructueuse ou d'un tag. Au lieu de rediriger l’utilisateur vers la page de résultats de recherche et le laisser cliquer sur le lien de l’article, on va directement le rediriger vers l’article correspondant à sa recherche. Ce plugin s'appuie sur une astuce soumise par Jean-David DAVIET (http://goo.gl/TpXl0G).
   Plugin URI:   http://goo.gl/cMVVY7
-  Version:      1.1
+  Version:      1.2
   Author:       Yvan GODARD
   Author URI:   http://www.yvangodard.me
   Donate link:  https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=HJVNXAKDZ5WKE
+  License:      GNU GPL V2 - https://www.gnu.org/licenses/gpl-2.0.html
 /*
 This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
 This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
@@ -14,7 +15,7 @@ You should have received a copy of the GNU General Public License along with thi
 Free Software Foundation, Inc. 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 */
 
-include_once('/inc/updater.php');
+include( 'updater.php' );
 
 add_action('template_redirect', 'redirect_search_to_single_post_result');
  
@@ -37,8 +38,8 @@ if (is_admin()) { // note the use of is_admin() to double check that this is hap
         'raw_url' => 'https://raw.github.com/yvangodard/WP-redirect-to-post-if-only-one-result/master', // the github raw url of your github repo
         'github_url' => 'https://github.com/yvangodard/WP-redirect-to-post-if-only-one-result', // the github url of your github repo
         'zip_url' => 'https://github.com/yvangodard/WP-redirect-to-post-if-only-one-result/archive/master.zip', // the zip url of the github repo
-        'sslverify' => 'true', // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
-        'requires' => '3.6', // which version of WordPress does your plugin require?
+        'sslverify' => true, // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+        'requires' => '3.5', // which version of WordPress does your plugin require?
         'tested' => '3.8', // which version of WordPress is your plugin tested up to?
         'readme' => 'actual_version.txt', // which file to use as the readme for the version number
         'access_token' => '', // Access private repositories by authorizing under Appearance > Github Updates when this example plugin is installed
