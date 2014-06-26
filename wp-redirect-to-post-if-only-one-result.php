53d3460c84885a7a0a68cde942a579b7ca697eb2
Yvan GODARD <godardyvan@gmail.com>
2014-06-26 15:52:10 +0200

    Version 1.1

--- wp-redirect-to-post-if-only-one-result.php
+++ wp-redirect-to-post-if-only-one-result.php
@@ -37,7 +37,7 @@ if (is_admin()) { // note the use of is_admin() to double check that this is hap
         'raw_url' => 'https://raw.github.com/yvangodard/WP-redirect-to-post-if-only-one-result/master', // the github raw url of your github repo
         'github_url' => 'https://github.com/yvangodard/WP-redirect-to-post-if-only-one-result', // the github url of your github repo
         'zip_url' => 'https://github.com/yvangodard/WP-redirect-to-post-if-only-one-result/archive/master.zip', // the zip url of the github repo
-        'sslverify' => true // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+        'sslverify' => 'true', // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
         'requires' => '3.6', // which version of WordPress does your plugin require?
         'tested' => '3.8', // which version of WordPress is your plugin tested up to?
         'readme' => 'actual_version.txt', // which file to use as the readme for the version number
